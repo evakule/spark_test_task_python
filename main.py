5521f5de316c916b76a5f18a50f9625b4344ab23
e5414 <guest.vakulenko.e@igt.com>
2021-03-01 11:53:32 +0200

    hot fixes

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from pyspark.sql import SparkSession
 from suicide_statistic_calculation import SuicideStatisticCalculation
 
-session = SparkSession.builder.appName("humanlves-pyspark").getOrCreate()
-data = session.read.csv(path='master.csv', sep=',', inferSchema=True, header=True)
+spark = SparkSession.builder.appName("humanlves-pyspark").getOrCreate()
+data = spark.read.csv(path='master.csv', sep=',', inferSchema=True, header=True)
 
 calculation = SuicideStatisticCalculation(data)
 
 
-print(calculation.get_top_five(data))
+print(calculation.get_top_five())
 
 
-calculation.get_avg_suicide_by_one_hundred(data).foreach(lambda x: print(x))
+calculation.get_avg_suicide_by_one_hundred().foreach(lambda x: print(x))
 
 
-calculation.get_country_age_distribution(data).foreach(lambda x: print(x))
+calculation.get_country_age_distribution().foreach(lambda x: print(x))
 
 
-calculation.get_country_year_to_suicide_amount_to_gdp(data).foreach(lambda x: print(x))
+calculation.get_country_year_to_suicide_amount_to_gdp().foreach(lambda x: print(x))
--- suicide_statistic_calculation.py
+++ suicide_statistic_calculation.py
@@ -1,35 +1,28 @@
 class SuicideStatisticCalculation:
+    rdd = None
 
     def __init__(self, data):
-        self.data = data
+        self.rdd = data.rdd
 
-    def get_top_five(self, data):
-        return data.rdd.map(lambda x: (x[7], x[4])) \
+    def get_top_five(self):
+        return self.rdd.map(lambda line: (line.country_year, line.suicides_no)) \
             .reduceByKey(lambda x, y: x + y) \
             .map(lambda x: (x[1], x[0])) \
             .sortByKey(ascending=False) \
             .map(lambda x: (x[1], x[0])).take(5)
 
-    def get_avg_suicide_by_one_hundred(self, data):
-        return data.rdd.map(lambda x: (x[7], (1, x[6]))) \
+    def get_avg_suicide_by_one_hundred(self):
+        return self.rdd.map(lambda line: (line.country_year, (1, line.suicides_100k_pop))) \
             .reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1])) \
             .map(lambda x: (x[0], x[1][1] / x[1][0])) \
             .map(lambda x: (x[1], x[0])) \
             .sortByKey(ascending=False) \
             .map(lambda x: (x[1], x[0]))
 
-    def output_refactor(self, row):
-        colon_index = row[0].index(':')
-        country = row[0][0:colon_index + 1]
-        years_and_amount_of_suicides = row[0][colon_index + 1:] + str(row[1])
-        return (country, years_and_amount_of_suicides)
+    def get_country_age_distribution(self):
+        return self.rdd.map(lambda x: (x.country + ': ' + x.age + ' = ', x.suicides_no)) \
+            .reduceByKey(lambda x, y: x + y)
 
-    def get_country_age_distribution(self, data):
-        return data.rdd.map(lambda x: (x[0] + ': ' + x[3] + ' = ', x[4])) \
-            .reduceByKey(lambda x, y: x + y) \
-            .map(lambda x: self.output_refactor(x)) \
-            .reduceByKey(lambda x, y: x + ',' + y)
-
-    def get_country_year_to_suicide_amount_to_gdp(self, data):
-        return data.rdd.map(lambda x: (x[7], (x[4], x[10]))) \
+    def get_country_year_to_suicide_amount_to_gdp(self):
+        return self.rdd.map(lambda x: (x.country_year, (x.suicides_no, x.gdp_per_capital))) \
             .reduceByKey(lambda x, y: (x[0] + y[0], x[1]))
