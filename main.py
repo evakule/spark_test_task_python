195ce6afe8cab26202585c66702c39c3a19592b5
e5414 <guest.vakulenko.e@igt.com>
2021-03-09 19:05:45 +0200

    adding writing to csv

--- main.py
+++ main.py
@@ -17,3 +17,6 @@ calculation.get_country_age_distribution().foreach(lambda x: print(x))
 
 
 calculation.get_country_year_to_suicide_amount_to_gdp().foreach(lambda x: print(x))
+
+df = spark.createDataFrame(calculation.get_avg_suicide_by_one_hundred())
+df.write.csv('output_package', header=True)
